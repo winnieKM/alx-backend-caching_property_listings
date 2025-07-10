170f7db633c3f17cd159b03d50c556b87693c23a
winniekathomi <kathomimemba@gmail.com>
2025-07-10 23:02:12 +0300

    Fix: Wrap property list in JsonResponse with 'data' key

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-@cache_page(60 * 15)  # Cache for 15 minutes
+@cache_page(60 * 15)  # Cache response for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values('id', 'title', 'description', 'price', 'location', 'created_at')
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({
+        "data": list(properties)  # ✅ wrap the list in a dictionary with key 'data'
+    })
